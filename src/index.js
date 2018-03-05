fccd8dc4411dd94f726aff0d3b221b15815d855b
ShustKristina <shust1991@mail.ru>
2018-03-05 08:31:06 +0300

    ready countZeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  
+  var countZeros=0;
+  var i=5;
+  while(number/i!=0){
+    countZeros+=Math.floor((number/i));
+    i=i*5;
+  }
+
+return countZeros;
 }
